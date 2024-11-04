40ece1666766aff75d8d7f9a1db78921c164967f
hanine007 <bouchibahanine@gmail.com>
2024-11-04 22:07:39 +0100

    addmorgan cors bodyparser route  Server

--- Seerver.js
+++ Seerver.js
@@ -1,11 +1,22 @@
-import express from 'express';
+import express, { urlencoded } from 'express';
+import router from './Routes/routes.js';
+import morgan from 'morgan';
+import cors from 'cors';
+import bodyParser from "body-parser";
+
+import { protection } from './modules/auth.js';
+import { creatUser, signin } from './handlers/User.js';
 
 const app = express();
+app.use (cors());
+app.use(morgan('dev'));
+app.use (urlencoded({extended:true}));
+app.use(bodyParser.json()); 
 
 app.get('/', (req, res) => {
     res.status(200).send('Hello World'); // Set status before sending response
 });
-
-
-
+app.use('/api',protection,router)
+app.post('/user',creatUser)
+app.post('/signin',signin)
 export default app;
